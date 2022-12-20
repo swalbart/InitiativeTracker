e5f22cff61a0d3b6cea7110b51e1c921ac6b1bb5
Tobi <tobias.zinke@gmx.net>
2022-12-20 20:49:46 +0100

    Initial Commit

---
+++ InitiativeTracker/InitiativeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InitiativeTrackerApp.swift
+//  InitiativeTracker
+//
+//  Created by Tobias Zinke on 20.12.22.
+//
+
+import SwiftUI
+
+@main
+struct InitiativeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
