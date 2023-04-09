6cb77fb54fcc5789877d693efe041e167a9ee425
Tobi <tobias.zinke@gmx.net>
2023-04-09 22:09:49 +0200

    Bugfix and removed Buttons
    Fixed a bug where the Displaybox always resets itself after deleting an entity.
    Removed previously planed save-button.
    Removed previously planed load-button.

--- InitiativeTracker/ViewControllerBoard.swift
+++ InitiativeTracker/ViewControllerBoard.swift
@@ -373,11 +373,17 @@ extension ViewControllerBoard: UITableViewDataSource{
     
     // delete entity in entities-array and cell in tableview
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        var deleteDisplayedEntity = false
+        if indexPath.row == currentPosition {
+            deleteDisplayedEntity = true
+        }
         if editingStyle == .delete{
             entityData.groupA.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
-        resetDisplayValues()
+        if deleteDisplayedEntity{
+            resetDisplayValues()
+        }
         entityData.save()
     }
     
